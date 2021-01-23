fc444c2d24d5d3352cc1dd6866b76f5c85c05827
Emerson Rocha <rocha@ieee.org>
2021-01-23 03:39:32 -0300

    joomla-tags.sql v1.1 (#3); created joomla-tabledump-tags.sql and joomla-tabledump-users.sql

--- sql/joomla-tags.sql
+++ sql/joomla-tags.sql
@@ -1,31 +1,33 @@
-
-
 -- ID:             joomla-tags
--- DESCRIPTION:    Return basic generic information about tags
+-- DESCRIPTION:    Return information about each tag and related general result
+--                 of tagged articles via `content_count` and `content_hits`
 --
 -- AUTHOR:         Emerson Rocha <emerson[at]alligo.com.br>
 -- COMPANY:        Alligo
 -- LICENSE:        "SPDX-License-Identifier: MIT"
+-- VERSION:        1.1
 -- CREATED:        2021-01-18 22:00 BRT
--- REVISION:       ---
+-- REVISION:       2021-01-23 06:19 UTC 1.1 Added tag_hits and content_hits
 
 SELECT
-    `tag`.`id` AS `tag_id`,
-    `tag`.`published` AS `tag_published`,
-    `tag`.`parent_id` AS `tag_parent_id`,
-    `tag`.`lft` AS `tag_lft`,
-    `tag`.`rgt` AS `tag_rgt`,
-    `tag`.`level` AS `tag_level`,
-    `tag`.`path` AS `tag_path`,
-    `tag`.`hits` AS `tag_hits`,
-    `tag`.`title` AS `tag_title`,
-    `tag`.`alias` AS `tag_alias`,
-    `tag`.`note` AS `tag_note`,
-    `tag`.`created_user_id` AS `tag_created_user_id`,
-    `tag`.`created_time` AS `tag_created_time`,
-    `tag`.`modified_user_id` AS `tag_modified_user_id`,
-    `tag`.`modified_time` AS `tag_modified_time`,
-    COUNT(`content`.`id`) AS `content_count`
+    `tag`.`id` AS `tag_id`
+    , `tag`.`published` AS `tag_published`
+    , `tag`.`parent_id` AS `tag_parent_id`
+    -- , `tag`.`lft` AS `tag_lft`
+    -- , `tag`.`rgt` AS `tag_rgt`
+    , `tag`.`level` AS `tag_level`
+    , `tag`.`path` AS `tag_path`
+    -- , `tag`.`title` AS `tag_title`
+    , `tag`.`alias` AS `tag_alias`
+    -- , `tag`.`note` AS `tag_note`
+    , `tag`.`created_user_id` AS `tag_created_user_id`
+    , `tag`.`created_time` AS `tag_created_time`
+    , `tag`.`modified_user_id` AS `tag_modified_user_id`
+    , `tag`.`modified_time` AS `tag_modified_time`
+    , COUNT(`content`.`id`) AS `content_count`
+    -- Since Weka, by defailt, will consider last item as Class/Target, we choose to leave tags_hits and content_hits last
+    , `tag`.`hits` AS `tag_hits`
+    , SUM(`content`.`hits`) AS `content_hits`
 FROM
     `JOSTABLEPREFIX_tags` AS `tag`
 LEFT JOIN `JOSTABLEPREFIX_contentitem_tag_map` AS `contentitem_tag_map` ON
---
+++ sql/tabledump/joomla-tabledump-tags.sql
@@ -0,0 +1,18 @@
+
+-- ID:             joomla-tabledump-tags
+-- DESCRIPTION:    This is a raw, table dump of the Joomla table tags.
+--                 All joomla-table dump- files are NOT meant to be used as main
+--                 reference for data mining, but they may be useful for quick
+--                 and simply copy. Also the name of the file can be used as
+--                 reference when there is a need to save data.
+-- 
+-- AUTHOR:         Emerson Rocha <emerson[at]alligo.com.br>
+-- COMPANY:        Alligo
+-- LICENSE:        "SPDX-License-Identifier: MIT"
+-- CREATED:        2021-01-23 05:57 BRT
+-- REVISION:       ---
+
+SELECT
+    *
+FROM
+    `JOSTABLEPREFIX_tags` AS `tags`
